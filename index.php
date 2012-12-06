4a34059900edf6a0ec8890db96ff306b1ad3675e
Jacob <kontakt@jacob-lassen.dk>
2012-12-06 14:28:01 +0100

    tiltøjet stylesheet.css og includet header.html

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-
 		<title>Tivoli Karneval</title>
-
+		<link rel="stylesheet" href="stylesheet.css" type="text/css" />
 	</head>
 
 	<body>
-
+		<?php
+			include 'header.html';
+		?>
 	</body>
 </html>
