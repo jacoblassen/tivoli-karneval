79a237f9ca2fd8ba3f20109e467d8b69e9fea40f
MikeSimony <mikejensen_5@hotmail.com>
2012-12-10 21:01:07 +0100

    Kontakt added + CSS updated

--- kontakt.php
+++ kontakt.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Information om Tivolis karneval</title>
+<title>Kontakt os</title>
 <link rel="stylesheet" href="cssreset.css" type="text/css" />
 <link rel="stylesheet" href="stylesheet.css" type="text/css" />
 <link href='http://fonts.googleapis.com/css?family=Old+Standard+TT' rel='stylesheet' type='text/css'>
@@ -15,5 +15,53 @@
 <?php
 	include 'lady.html'
 ?>
+	<div id="master">
+    
+          	<div id="kontaktWrap"> 
+            	<h1>Title</h1>
+                
+                
+            	<div class="kontaktPers"><img class="kontaktPic" src="" />
+                	<p>Info text</p>
+                </div>
+                
+                <div class="kontaktPers"><img class="kontaktPic" src="" />
+                	<p>Info text</p>
+                </div>
+                
+              <div class="kontaktPers"><img class="kontaktPic" src="" />
+                	<p>Info text</p>
+                </div>
+                
+                <div class="kontaktPers"><img class="kontaktPic" src="" />
+                	<p>Info text
+                    info text 2
+                  info tetx 3</p>
+              </div>
+     	
+            </div><!-- kontaktWrap -->
+            
+            <div id="formWrap">
+             
+             		<div id="kontaktForm">
+                    	<h1>Kontakt os</h1>
+                    	<form>
+                        	<input type="text" name="fname"/>
+                            <input type="text" name="lname"/>
+                            <input type="text" name="addr"/>
+                            <input type="text" name="zip"/>
+                        </form>
+                    
+                		</div>
+            	</div><!-- formWrap -->
+             
+             	
+            
+    
+    
+    </div> <!-- master -->
+
+	
+
 </body>
 </html>
