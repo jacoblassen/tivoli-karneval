6662d0b13a114f7b78002de17b5b89869e89ee18
Jacob <kontakt@jacob-lassen.dk>
2012-12-10 18:24:24 +0100

    js validation mangler at validere addresse

--- javascript.js
+++ javascript.js
@@ -17,3 +17,52 @@ function tilmeldClose()	{
 function tilmeldProg(ev)	{
 		ev.stopPropagation();
 }
+
+
+//Validation
+
+function validateForm()	{
+	
+	document.forms['form']['fname'].style.backgroundColor = "#fff";
+	document.forms['form']['lname'].style.backgroundColor = "#fff";
+	document.forms['form']['email'].style.backgroundColor = "#fff";
+	document.forms['form']['addr'].style.backgroundColor = "#fff";
+	document.forms['form']['zip'].style.backgroundColor = "#fff";
+	
+	var error = 0;
+	var fname = document.forms['form']['fname'].value;
+	var lname = document.forms['form']['lname'].value;
+	var email = document.forms['form']['email'].value;
+	var addr = document.forms['form']['addr'].value;
+	var zip = document.forms['form']['zip'].value;
+	var news = document.forms['form']['news'].checked;
+	
+	if(!(fname.match(/^[a-zA-ZæøåÆØÅ]+$/))){
+		error = 1;
+		document.forms['form']['fname'].style.backgroundColor = "#f06565";
+	}
+	
+	if(!(lname.match(/^[a-zA-ZæøåÆØÅ]+$/))){
+		error = 1;
+		document.forms['form']['lname'].style.backgroundColor = "#f06565";
+	}
+	
+	if(!(email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}$/))){
+		error = 1;
+		document.forms['form']['email'].style.backgroundColor = "#f06565";
+	}
+	
+	if(!(zip.match(/^\d{4}$/))){
+		error = 1;
+		alert(zip)
+		document.forms['form']['zip'].style.backgroundColor = "#f06565";
+	}
+	
+	if(error == 1){
+		return false;
+	}
+	if(error == 0){
+		return true;
+	}
+	
+}
