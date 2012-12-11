9051756a91a0847fbaabe3a5b8763a14176baabd
Jacob <kontakt@jacob-lassen.dk>
2012-12-11 11:11:43 +0100

    oprettet forbindelse til local database og kan komme brugere ind

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+    $link = new mysqli("localhost", "root", "", "karneval");
+	if($link->connect_error)	{
+		die('Connect Error('.$link->connect_errno.') '.$link->connect_error);
+	}
+?>
--- insert.php
+++ insert.php
@@ -1,10 +1,10 @@
 <?php
+	include 'dbcon.php';
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
 	$addr = $_POST['addr'];
 	$zip = $_POST['zip'];
 	$email = $_POST['email'];
-	echo $fname;
 	
 	if (!preg_match("/^[a-zA-ZæøåÆØÅ ]{2,}$/", $fname))	{
 		echo "fname er skrevet i forkert format <br />";
@@ -25,4 +25,8 @@
 		echo "addr er skrevet i forkert format <br />";
 		return; //Stop further execution
 	}
+	
+	$stmt = $link->prepare("INSERT INTO bruger (fornavn, efternavn, addresse, email, postnr) VALUES (?,?,?,?,?)");
+	$stmt->bind_param("ssssi", $fname, $lname, $addr, $email, $zip);
+	$stmt->execute();
 ?>
