56603fd7f7899a84a09e41852420006d2cee7c79
Jacob <kontakt@jacob-lassen.dk>
2012-12-11 12:25:59 +0100

    php insert checker nu værdien af nyhedsbrev

--- insert.php
+++ insert.php
@@ -5,28 +5,41 @@
 	$addr = $_POST['addr'];
 	$zip = $_POST['zip'];
 	$email = $_POST['email'];
+	$error = 1;
+	
+	if(isset($_POST["news"])){
+		$news = 1;
+	}
+	else{
+		$news = 2;
+	}
 	
 	if (!preg_match("/^[a-zA-ZæøåÆØÅ ]{2,}$/", $fname))	{
 		echo "fname er skrevet i forkert format <br />";
-		return; //Stop further execution
+		$error = 2;
 	}	
 	
 	if (!preg_match("/^[a-zA-ZæøåÆØÅ ]{2,}$/", $lname))	{
 		echo "lname er skrevet i forkert format <br />";
-		return; //Stop further execution
+		$error = 2;
 	}	
 	
 	if (!preg_match("/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}$/", $email))	{
 		echo "email er skrevet i forkert format <br />";
-		return; //Stop further execution
+		$error = 2;
 	}	
 	
 	if (!preg_match("/^[a-zA-ZæøåÆØÅ ]* [0-9]{1,30}( ?)[a-zA-ZæøåÆØÅ0-9\.]*$/", $addr))	{
 		echo "addr er skrevet i forkert format <br />";
-		return; //Stop further execution
+		$error = 2;
 	}
 	
-	$stmt = $link->prepare("INSERT INTO bruger (fornavn, efternavn, addresse, email, postnr) VALUES (?,?,?,?,?)");
-	$stmt->bind_param("ssssi", $fname, $lname, $addr, $email, $zip);
+	if ($error == 2){
+		echo "fejl";
+		return;
+	}
+	
+	$stmt = $link->prepare("INSERT INTO bruger (fornavn, efternavn, addresse, email, postnr, nyhedsbrev) VALUES (?,?,?,?,?,?)");
+	$stmt->bind_param("ssssis", $fname, $lname, $addr, $email, $zip, $news);
 	$stmt->execute();
 ?>
